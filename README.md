77d0fa7e2c985ccd42e44f27ddd312b72ea5dd50
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-03-09 09:33:25 +0100

    Better README, added doc comments, some refactorings

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 > When you absolutely, positively have to generate 
 > HTML within PHP Code.
 
-Jazz is a very tiny library for turning a specially
-structured Array into HTML. You could call it a "DSL" if you
-want.
+Jazz is a very tiny library which turns a nested Array into HTML. 
+You could call it a "DSL" if you want. It works well with PHP later
+than 5.3.3 but looks best with 5.4's new short array syntax.
 
 Here is a "Hello World" (_in PHP 5.4_):
 
@@ -16,7 +16,7 @@ Here is a "Hello World" (_in PHP 5.4_):
     $html = [
         ["#h1", "Hello World"],
         ["#p", ["class" => "intro"], [
-            "This is some content for this <p> element.",
+            "This is some content for this element.",
             "Here is a second line.",
 
             ["#a", ["href" => "https://github.com/CHH/Jazz"], [
@@ -27,26 +27,41 @@ Here is a "Hello World" (_in PHP 5.4_):
 
     echo Jazz::render($html);
     # <h1>Hello World</h1>
-    # <p>This is some content for this &lt;p&gt; element.
+    # <p>This is some content for this element.
     # Here is a second line.
     # <a href="https://github.com/CHH/Jazz">This is a link.</a>
     # </p>
-
-Jazz works perfectly fine with PHP 5.3, but due to 5.4's new 
-short array notation it's more readable with PHP 5.4.
 
 ## Syntax
 
 ### Tags
 
-A tag is an array of two or three elements: `["#$tagName", $attributes,
-$body]`. The first element is the HTML tag's name, prefixed with a pound
-sign ("#"). Attributes are passed as Array at index `1`. Attributes are
-optional. Finally, the body is the third element in the Array and can
-either be a single String, or an Array with sub elements.
+A tag is an array of up to three elements (where `$attributes` and
+`$body` are optional): 
+
+    ["#$tagName", $attributes, $body]
+
+ * `"#$tagName"`, __mandatory__: 
+   The first element is the name of the HTML tag, 
+   prefixed with a `#`. This is the only mandatory thing for the tag.
+ * `$attributes`, __optional__:
+   Attributes are an optional array, passed as second entry. Each key in
+   the array is the attribute name.
+ * `$body`:
+   The body is either a String or an Array of sub elements.
 
 When attributes are left out, the _second_ element in the tag is treated
 as body.
+
+Tags can be written in different forms, just like in HTML:
+
+ * Short. For example `["#br"]` becomes `<br />`.
+ * Short, with Attributes. For example `["#img", ["src" => "/foo/bar.png"]]` 
+   becomes `<img src="/foo/bar.png" />`.
+ * Short, with Body: For example `["#p", "Foo"]` becomes
+   `<p>Foo</p>`.
+ * Long Form: For example `["#h1", ["role" => "banner"], "Hello World"]`
+   becomes `<h1 role="banner">Hello World</h1>`.
 
 Jazz makes self-closed (`/>`) tags when no body is passed.
 
@@ -69,4 +84,9 @@ Load composer's class loader:
     <?php
 
     require_once("vendor/.composer/autoload.php");
+
+## License
+
+Jazz is licensed under the MIT license, bundled with the Source Code in
+the file `LICENSE.txt` as well as contained in the class file.
 
--- lib/Jazz.php
+++ lib/Jazz.php
@@ -1,13 +1,65 @@
 <?php
+/*
+The MIT License
 
+Copyright (c) 2012 Christoph Hochstrasser
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+# Converts nested arrays to HTML.
+#
+# Examples
+# 
+#   echo Jazz::render(["#h1", "It works"]);
+#   # => "<h1>It works</h1>"
+#
+#   echo Jazz::render(["#br"]);
+#   # => "<br />"
+#
+#   echo Jazz::render(["#img", ["src" => "/foo.png"]]);
+#   # => "<img src="/foo.png" />"
+#
+#   $ul = ["#ul", []];
+#
+#   foreach (array("Foo", "Bar", "Baz") as $item) {
+#     $ul[1][] = ["#li", $item];
+#   }
+#
+#   echo Jazz::render($ul);
+#   # => "<ul><li>Foo</li><li>Bar</li><li>Baz</li></ul>"
+#
 class Jazz
 {
+    # Renders the given node. A node can actually be a single tag,
+    # text content or a list of sub nodes.
+    #
+    # node - Tag as Array, Array of sub nodes or a single stringable 
+    #        element.
+    # 
+    # Returns HTML as String.
     static function render($node)
     {
         $out = "";
 
         if (is_array($node)) {
-            if (is_string($node[0]) and substr($node[0], 0, 1) == "#") {
+            if (static::isTag($node)) {
                 $out .= static::renderHtmlTag($node);
             } else {
                 foreach ($node as $n) {
@@ -21,15 +73,28 @@ class Jazz
         return $out;
     }
 
+    protected static function isTag($node)
+    {
+        return is_string(@$node[0]) and substr($node[0], 0, 1) == "#";
+    }
+
+    # Renders a HTML Tag.
+    #
+    # node - Tag as Array.
+    #
+    # Returns the HTML as String.
     protected static function renderHtmlTag($node)
     {
         # Strip the leading "#"
         $tagName    = substr(array_shift($node), 1);
         $attributes = array();
 
+        # Short Tag
         if (sizeof($node) == 1) {
-            if (is_array($node[0])) {
-                is_string(key($node[0])) ? $attributes = $node[0] : $content = $node[0];
+            # Look if the second element is an associative attributes
+            # array or a body.
+            if (is_array($node[0]) and is_string(key($node[0]))) {
+                $attributes = $node[0];
             } else {
                 $content = $node[0];
             }

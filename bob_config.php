eeef9a3b0c021e7617d50812a573b9d562b32234
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-03-09 09:37:20 +0100

    Added task for README testing

--- bob_config.php
+++ bob_config.php
@@ -9,6 +9,12 @@ task("test", array("phpunit.xml"), function() {
     sh("phpunit");
 });
 
+desc("Converts the README to markdown and opens the result in the browser.");
+task("readme", function() {
+    `redcarpet README.md > README.html`;
+    `open README.html`;
+});
+
 fileTask("phpunit.xml", array("phpunit.dist.xml"), function() {
     copy("phpunit.dist.xml", "phpunit.xml");
 });
